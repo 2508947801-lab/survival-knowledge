0e018b4334c3fd1116167b011a0888e63b6d32be
Nean <thea@NeanMacBook.local>
2026-08-24 12:11:06 +0800

    📦 管理系统同步 2026-08-24 12:11

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-/* Yara PWA service worker. 20260824120731 is replaced by the release script. */
-const YARA_BUILD = '20260824120731';
+/* Yara PWA service worker. 20260824121105 is replaced by the release script. */
+const YARA_BUILD = '20260824121105';
 const CACHE_PREFIX = 'yara-pwa-';
 const CACHE_NAME = CACHE_PREFIX + YARA_BUILD;
 const OFFLINE_URL = './离线.html';
